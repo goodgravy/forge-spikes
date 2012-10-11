79bfc80079e7e4e5fdaf9be251563ee04e756a09
James Brady <james.colin.brady@gmail.com>
2012-10-11 10:52:32 +0100

    remove unused resources and update code to download remote pdf

--- pdf-reader/src/js/main.js
+++ pdf-reader/src/js/main.js
@@ -1,7 +1,12 @@
-setTimeout(function () {
-	forge.tools.getURL('google.pdf',
-		function (file) {
-			 forge.tabs.open(file);
-		}
+window.onload = function () {
+	forge.file.cacheURL('http://ilpubs.stanford.edu:8090/361/1/1998-8.pdf',
+			function (file) {
+				forge.file.URL(file, function (url) {
+					forge.tabs.open(url);
+				});
+			},
+			function (error) {
+				forge.logging.error(JSON.stringify(error));
+			}
 	);
-}, 2000);
+};
